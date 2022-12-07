2bd6b272f8d9921b2ee6158deec377697a54ef64
Avri <yoti1492@gmail.com>
2022-12-08 00:19:33 +0200

    __tests__

--- src/Components/SortOrFilter/FilterBySearch.jsx
+++ src/Components/SortOrFilter/FilterBySearch.jsx
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const FilterBySearch = ({ setFilterOrsort }) => {
+
+  
+  const [input, setInput] = useState("");
+
   return (
     <>
       <label style={{ textAlign: "center", display: "grid" }}>
         Search for employees by name :
         <input
+          value={input}
           type="text"
           placeholder="Start Search .."
           style={{ width: "22vw", marginTop: "1vw" }}
           onChange={(e) => {
             setFilterOrsort({ type: "SearchIn", by: e.target.value });
+            setInput(e.target.value);
+          }}
+          onBlur={() => {
+            setFilterOrsort({ type: "default", by: "default" });
+            setInput("");
           }}
         />
       </label>
@@ -20,7 +31,7 @@ const FilterBySearch = ({ setFilterOrsort }) => {
 };
 
 FilterBySearch.propTypes = {
-  setFilterOrsort: PropTypes.func
+  setFilterOrsort: PropTypes.func,
 };
 
 export default FilterBySearch;
